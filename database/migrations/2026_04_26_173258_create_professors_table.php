14cb4dba57c7f109d2bae31a31a4afe6c95881a4
Jefferson Henrique Alves Dos Santos <210017@upf.br>
2026-05-06 21:33:27 -0300

    06/05 atualizado para php 8.5 em aula

--- database/migrations/2026_04_26_173258_create_professors_table.php
+++ database/migrations/2026_04_26_173258_create_professors_table.php
@@ -14,6 +14,10 @@ return new class extends Migration
         Schema::create('professors', function (Blueprint $table) {
             $table->id();
             $table->string('nome');
+            $table->string('email')->unique();
+            $table->string('departamento');
+            $table->string('telefone')->nullable();
+            $table->text('biografia')->nullable();
             $table->timestamps();
         });
     }
